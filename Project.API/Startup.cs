70c4ce73362864784dc88ce46ff27188028e3fd0
dujl <dujl@tututech.net>
2018-05-08 15:11:47 +0800

    Project.API.csproj 集成 mediatR 通讯

--- Project.API/Startup.cs
+++ Project.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,9 @@ namespace Project.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddMediatR();
+
             services.AddMvc();
         }
 
