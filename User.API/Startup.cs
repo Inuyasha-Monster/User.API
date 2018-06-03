c026ca885117f075f9f1a2fc96fce5eb3401eb7d
djlnet <394922860@qq.com>
2018-06-04 00:40:53 +0800

    test git commit

--- User.API/Startup.cs
+++ User.API/Startup.cs
@@ -34,6 +34,8 @@ namespace User.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // test git commit on new computer
+
             services.AddDbContext<UserDbContext>(builder =>
             {
 
